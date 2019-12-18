1c06f0b033a843e8fa1723ff32a51bd3ae052ab1
PovilasU <povilas.urbonas85@gmail.com>
2019-12-18 20:05:35 +0100

    added ReadMore, showLess buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Povilas Urbonas Portfolio
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+    console.log("Component this", this);
+
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Povilas. I'am a software engineer.</p>
+        <p>I'm always looking forward to work on meaningful projects.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Oslo, and code every day.</p>
+            <p>
+              My favorite language is JavaScript, and I this React.js is
+              awesome.
+            </p>
+            <p>Besides coding, I also love movies and music!</p>
+            <button onClick={this.toggleDisplayBio}>Show less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
