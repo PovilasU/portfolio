f021d82e003f2a68ee518be3b0bfbf5963e865ed
PovilasU <povilas.urbonas85@gmail.com>
2019-12-18 22:10:14 +0100

    Updates

--- precache-manifest.6ab54ee4ef97d51ada87e749560dcecd.js
+++ precache-manifest.6ab54ee4ef97d51ada87e749560dcecd.js
@@ -1,7 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca39bd60e9c224706868f3040fe9a8c7",
+    "revision": "05651d70b3d2582e3438a604d1e4c542",
     "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "933dfd1bd3a835b1dc3f",
+    "url": "/portfolio/static/css/main.8d1aeb1a.chunk.css"
   },
   {
     "revision": "10294578b5c52bc5f9b1",
@@ -12,12 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4aa2e922.chunk.js.LICENSE"
   },
   {
-    "revision": "3c22f86990c9cdf540a8",
-    "url": "/portfolio/static/js/main.7e2a2902.chunk.js"
+    "revision": "933dfd1bd3a835b1dc3f",
+    "url": "/portfolio/static/js/main.33cf8c82.chunk.js"
   },
   {
     "revision": "8575fab3f3395c90d845",
     "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
   },
   {
     "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
@@ -42,6 +50,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7bded960f9e6263b998eb4529af6e041",
     "url": "/portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
   },
   {
     "revision": "512536cb3a4583052bd81fbf9760521f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.32f93336b9a93cf815ebf25d40f15262.js"
+  "/portfolio/precache-manifest.6ab54ee4ef97d51ada87e749560dcecd.js"
 );
 
 self.addEventListener('message', (event) => {
