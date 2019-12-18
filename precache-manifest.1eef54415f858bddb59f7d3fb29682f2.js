a5551aaa4ea3a883c3ac6020c44ee0569fd906cf
PovilasU <povilas.urbonas85@gmail.com>
2019-12-18 20:54:53 +0100

    Updates

---
+++ precache-manifest.1eef54415f858bddb59f7d3fb29682f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328b9a6be8ef67eed3bbba8c880a5db6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a15e3ee6939e60460c31",
+    "url": "/portfolio/static/js/2.9036f1fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.9036f1fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09380c45acbe2e45bfb7",
+    "url": "/portfolio/static/js/main.196ff1aa.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/portfolio/static/media/project3.7bded960.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.914a3b0b29f941e16ae11b1a171b1e9e.js"
+  "/portfolio/precache-manifest.1eef54415f858bddb59f7d3fb29682f2.js"
 );
 
 self.addEventListener('message', (event) => {
