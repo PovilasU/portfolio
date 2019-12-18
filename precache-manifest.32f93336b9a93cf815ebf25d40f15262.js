f55214587adb91d1717a7b3b75a13dff1caf8167
PovilasU <povilas.urbonas85@gmail.com>
2019-12-18 21:53:11 +0100

    Updates

---
+++ precache-manifest.32f93336b9a93cf815ebf25d40f15262.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca39bd60e9c224706868f3040fe9a8c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10294578b5c52bc5f9b1",
+    "url": "/portfolio/static/js/2.4aa2e922.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.4aa2e922.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c22f86990c9cdf540a8",
+    "url": "/portfolio/static/js/main.7e2a2902.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/portfolio/static/media/twitter_icon.512536cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.db35adfab50bfe4e755aea068ebc3256.js"
+  "/portfolio/precache-manifest.32f93336b9a93cf815ebf25d40f15262.js"
 );
 
 self.addEventListener('message', (event) => {
